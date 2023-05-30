3b369b84abbe056052022dd90fac108c57263de1
leilafaez <leila.faez@gmail.com>
2023-05-30 13:22:17 +0100

    implement resource list component

--- components/ResourceHighlight.js
+++ components/ResourceHighlight.js
@@ -1,41 +1,41 @@
 const ResourceHighlight = () => {
   return (
     <section class="hero ">
-        <div class="hero-body">
-        <div class="container">
-            <section class="section">
-            <div class="columns">
-                <div class="column is-8 is-offset-2">
-                <div class="content is-medium">
-                    <h2 class="subtitle is-4">May 25, 2023</h2>
-                    <h1 class="title">Getting Started</h1>
-                    <p>
-                    This is a starter template for creating a beautiful,
-                    customizable blog with minimal effort.{" "}
-                    </p>
-                </div>
-                </div>
+    <div class="hero-body">
+    <div class="container">
+        <section class="section">
+        <div class="columns">
+            <div class="column is-8 is-offset-2">
+            <div class="content is-medium">
+                <h2 class="subtitle is-4">May 25, 2023</h2>
+                <h1 class="title">Getting Started</h1>
+                <p>
+                This is a starter template for creating a beautiful,
+                customizable blog with minimal effort.{" "}
+                </p>
             </div>
-            </section>
-
-            <div class="is-divider"></div>
-
-            <section class="section">
-            <div class="columns">
-                <div class="column is-8 is-offset-2">
-                <div class="content is-medium">
-                    <h2 class="subtitle is-4">May 25, 2023</h2>
-                    <h1 class="title">Getting Started</h1>
-                    <p>
-                    This is a starter template for creating a beautiful,
-                    customizable blog with minimal effort.
-                    </p>
-                </div>
-                </div>
             </div>
-            </section>
         </div>
+        </section>
+
+        <div class="is-divider"></div>
+
+        <section class="section">
+        <div class="columns">
+            <div class="column is-8 is-offset-2">
+            <div class="content is-medium">
+                <h2 class="subtitle is-4">May 25, 2023</h2>
+                <h1 class="title">Getting Started</h1>
+                <p>
+                This is a starter template for creating a beautiful,
+                customizable blog with minimal effort.
+                </p>
+            </div>
+            </div>
         </div>
+        </section>
+    </div>
+    </div>
     </section>
   );
 };
