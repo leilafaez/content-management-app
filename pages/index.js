473a55ee16a23f337eeb4d4ccc94f1d7aec2c67d
leilafaez <leila.faez@gmail.com>
2023-05-30 13:29:21 +0100

    fix all the components in index.js file

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
-
+import Navbar from "@/components/Navbar";
+import ResourceHighlight from "@/components/ResourceHighlight";
+import Newsletter from "@/components/Newsletter";
+import ResourceList from "@/components/ResourceList";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-    
-      
+    <Navbar />
+    <ResourceHighlight/>
+    <Newsletter/>
+    <ResourceList/>
+    <Footer/>
 
-      
-      
-
-      
-      
-
-      
-
-      
-
-      
     </>
+
    
   )
 }
