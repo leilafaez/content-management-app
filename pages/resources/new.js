c17af605c8e880cfeac15aab38dbf92ff0a2eb7c
leilafaez <leila.faez@gmail.com>
2023-06-01 16:09:44 +0100

    improve the form

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -1,7 +1,16 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
+
+const DEFAULT_DATA =  {
+    title : "",
+    description : "",
+    link:"",
+    priority : "2",
+    timeToFinish : 60
+}
 
 const ResourceCreate = ()=>{
-
+    const [form,setForm] =useState(DEFAULT_DATA);
     return (
       <Layout>
         <div className="container">
@@ -9,18 +18,74 @@ const ResourceCreate = ()=>{
             <div className="column is-8 is-offset-2">
               <div className="resource-form">
                 <h1 className="title">Add New Resource</h1>
-                    <form>
-                    <div className="field">
-                        <label className="label">Name</label>
-                        <div className="control">
-                        <input
-                            className="input"
-                            type="text"
-                            placeholder="Text input"
-                        />
-                        </div>
+                <form>
+                  <div className="field">
+                    <label className="label">Title</label>
+                    <div className="control">
+                      <input
+                        value={form.title}
+                        className="input"
+                        type="text"
+                        placeholder="Add title"
+                      />
                     </div>
-                    </form>
+                  </div>
+                  <div className="field">
+                    <label className="label">Description</label>
+                    <div className="control">
+                      <textarea
+                        value={form.description}
+                        class="textarea"
+                        placeholder="Add description"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Link</label>
+                    <div className="control">
+                      <input
+                        value={form.link}
+                        className="input"
+                        type="text"
+                        placeholder="http://example.com"
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Priority</label>
+                    <div className="control">
+                      <div className="select">
+                        <select value={form.priority}>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Time to finish</label>
+                    <div className="control">
+                      <input
+                        value={form.timeToFinish}
+                        className="input"
+                        type="number"
+                        placeholder="60"
+                      />
+                    </div>
+                    <p className="help">Time is in minutes</p>
+                  </div>
+                  <div className="field is-grouped">
+                    <div className="control">
+                      <button className="button is-link">Submit</button>
+                    </div>
+                    <div className="control">
+                      <button className="button is-link is-light">
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
