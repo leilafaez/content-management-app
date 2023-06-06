920851e9ac42dd38967941ca225066dcf10035ca
leilafaez <leila.faez@gmail.com>
2023-06-06 16:00:53 +0100

    fetch data by id in resource detail page

--- pages/resources/[id].js
+++ pages/resources/[id].js
@@ -1,32 +1,38 @@
 import Layout from "@/components/Layout"
 
-const ResourceDetail= ()=>{
-    return (
-      <Layout>
-        <section className="hero ">
+const ResourceDetail = ({ resource }) => {
+  return (
+    <Layout>
+      <section className="hero ">
         <div className="hero-body">
-        <div className="container">
-            
-                <section className="section">
-                <div className="columns">
-                    <div className="column is-8 is-offset-2">
-                    <div className="content is-medium">
-                        <h2 className="subtitle is-4">
-                        DATE HERE
-                        </h2>
-                        <h1 className="title">TITLE HERE</h1>
-                        <p>
-                        DESCRIPTION HERE
-                        </p>
-                    </div>
-                    </div>
+          <div className="container">
+            <section className="section">
+              <div className="columns">
+                <div className="column is-8 is-offset-2">
+                  <div className="content is-medium">
+                    <h2 className="subtitle is-4">{resource.createdAt}</h2>
+                    <h1 className="title">{resource.title}</h1>
+                    <p>{resource.description}</p>
+                  </div>
                 </div>
-                </section>
-                </div>
+              </div>
+            </section>
           </div>
-        </section>
-      </Layout>
-    );
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export async function getServerSideProps({params}){
+
+    const dataRes = await fetch(`http://localhost:3001/api/resources/${params.id}`);
+    const data = await dataRes.json();
+    return{
+        props : {
+            resource : data
+        }
+    }
 }
 
 export default ResourceDetail;
