afb1299f3139f0cd0a69f67af1e489194cb382aa
leilafaez <leila.faez@gmail.com>
2023-06-06 15:41:36 +0100

    implement resource detail fetch

--- components/ResourceHighlight.js
+++ components/ResourceHighlight.js
@@ -4,23 +4,21 @@ const ResourceHighlight = ({ resources }) => {
       <div className="hero-body">
         <div className="container">
           {resources.map(resource=>{
-            return(
-            <section key={resource.id} className="section">
-            <div className="columns">
-              <div className="column is-8 is-offset-2">
-                <div className="content is-medium">
-                  <h2 className="subtitle is-4">{resource.createdAt}</h2>
-                  <h1 className="title">{resource.title}</h1>
-                  <p>
-                    This is a starter template for creating a beautiful,
-                    customizable blog with minimal effort.
-                  </p>
+            return (
+              <section key={resource.id} className="section">
+                <div className="columns">
+                  <div className="column is-8 is-offset-2">
+                    <div className="content is-medium">
+                      <h2 className="subtitle is-4">{resource.createdAt}</h2>
+                      <h1 className="title">{resource.title}</h1>
+                      <p>
+                        {resource.description}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </section>
-          
-            )})}
+              </section>
+            );})}
           
         </div>
       </div>
