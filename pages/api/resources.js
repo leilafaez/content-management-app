48f5448680899cd8b4a33151277676166be09281
leilafaez <leila.faez@gmail.com>
2023-06-06 11:47:34 +0100

    install and use axios

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,6 +1,12 @@
 
 export default async function(req,res){
-    const dataRes = await fetch("http://localhost:3001/api/resources");
-    const data = await dataRes.json();
-    res.send(data)
+    if(req==="GET"){
+        const dataRes = await fetch("http://localhost:3001/api/resources");
+        const data = await dataRes.json();
+        return res.send(data);
+    }
+    if(req==="POST"){
+       return res.send("data has been recieved!")
+    }
+    
 }
