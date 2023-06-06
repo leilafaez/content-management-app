695984965ed452bb2bfbbabcec17a0fc65bdd06a
leilafaez <leila.faez@gmail.com>
2023-06-06 12:25:39 +0100

    use axios for post method

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 export default async function(req,res){
     if(req.method ==="GET"){
@@ -10,6 +11,7 @@ export default async function(req,res){
         if(!title || !description|| !link|| !timeToFinish|| !priority){
              return res.status(422).send("Data are missing!");
         }
+            axios.post("http://localhost:3001/api/resources",req.body);
              return res.send("data has been recieved!")
     }
     
