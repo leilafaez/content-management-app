156d57c487c6b16e4bd2c9c585cdd579d2f5e355
leilafaez <leila.faez@gmail.com>
2023-06-09 11:56:08 +0100

    implement active resource component

--- components/ActiveResource.js
+++ components/ActiveResource.js
@@ -1,8 +1,23 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const ActiveResource =()=>{
+
+    const [resource,setResource]=useState({});
+
+    useEffect(()=>{
+       async function fetchResource(){
+        const res = await axios.get("/api/activeResource");
+        const resource = res.data;
+        setResource(resource)
+        }
+        fetchResource();
+    },[])
+
     return (
       <div className="active-resource">
-        <h1 className="resource-name">My active resource</h1>
+        <h1 className="resource-name">{resource.title}</h1>
         <div className="time-wrapper">
           <h2 className="elapsed-time">1400</h2>
         </div>
