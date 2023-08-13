2ef32b26248ceb38419a1ec0d02eee0aa81759c0
leilafaez <leila.faez@gmail.com>
2023-08-13 17:12:01 +0100

    edit footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        <p>&copy; 2023 Your Resource Management App. All rights reserved.</p>
+        <p>&copy; 2023 Your Resource Management App. </p>
         <p>
           Built with ❤️ using React.js, Next.js, and Simple Express API. Styled
           with Bulma.
