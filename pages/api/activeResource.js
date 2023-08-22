e3506bf65c80519e68c228085cbb37335db4e9fc
leilafaez <leila.faez@gmail.com>
2023-08-22 17:11:10 +0100

    fix issue

--- pages/api/activeResource.js
+++ pages/api/activeResource.js
@@ -4,5 +4,6 @@ export default async function activeResource(req,res){
     const axiosRes = await axios.get(`${process.env.API_URL}/activeResource`);
     const resource = axiosRes.data;
 
-    return res.send(resource);
+    return res.send(resource.data);
 }
+
